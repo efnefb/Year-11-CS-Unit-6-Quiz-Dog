2dd686c95b762f20ede9a91e1883c550c29e50f2
efnefb <ctwn43@student.cis.edu.hk>
2024-02-06 13:11:43 +0800

    Sem 2 Unit 1 Quiz 1

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,52 @@
 public class Dog {
+
+    private int age;
+    private String name;
+    private String color;
+    private Owner owner;
+
+    public Dog(int a, String n, String c, Owner o){
+        age = a;
+        name = n;
+        color = c;
+        owner = o;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String c){
+        color = c;
+    }
+    public Owner getOwner(){
+        return owner;
+    }
+    public void setOwner(Owner o){
+        owner = o;
+    }
+
+    public void addOneAge(){
+        age++;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+
+    public String toString(){
+        return "The dog is named " + name + " and is " + age + " years old. This dog is " + color;
+    }
+
 }
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,27 @@
 public class Owner {
+    private String firstName, lastName, phoneNumber;
+    public Owner(String fn, String ln, String pn){
+        firstName = fn;
+        lastName = ln;
+        phoneNumber = pn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String fn){
+        firstName = fn;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String ln){
+        lastName = ln;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String pn){
+        phoneNumber = pn;
+    }
 }
